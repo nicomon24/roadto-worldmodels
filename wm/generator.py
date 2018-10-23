b5aad80ff5e09aed69bd7b43c4113ab79457ebfc
nicomon24 <nico.montali24@gmail.com>
2018-10-23 20:00:28 +0200

    Added multiproc generator

--- wm/generator.py
+++ wm/generator.py
@@ -4,7 +4,7 @@
     Parallelized for multi-core execution.
 '''
 
-from multiprocessing import Pool
+from multiprocessing import Process, Queue, Event
 
 import gym, time, argparse, cv2, uuid, os
 from gym import wrappers, logger
@@ -15,32 +15,58 @@ import torch
 from wrappers import CropCarRacing, ResizeObservation, NormalizeRGB
 from utils import NCHW, NHWC, select_n_workers
 
-def generate_episode(make_env, make_model, seed, horizon):
-    # Make env
-    env = make_env(seed)
-    model = make_model()
-    # Aggregators
-    episode_observations = []
-    episode_actions = []
-    print("Here i am")
-    obs = env.reset()
-    print("Rocking like a hurricane")
-    # Iteration
-    for timestep in trange(horizon):
-        # Registering observation
-        episode_observations.append(obs)
-        # Getting action and step
-        a = env.action_space.sample()
-        obs, reward, done, info = env.step(a)
-        # Registering action
-        episode_actions.append(a)
-        # Check if done
-        if done:
-            break
-    # Numpify
-    episode_observations = np.array(episode_observations)
-    episode_actions = np.array(episode_actions)
-    return episode_observations, episode_actions
+
+
+class Worker(Process):
+
+    def __init__(self, input, output, event, make_env, make_model, horizon, seed):
+        super(Worker, self).__init__()
+        self.input = input
+        self.output = output
+        self.event = event
+        self.make_env = make_env
+        self.make_model = make_model
+        self.horizon = horizon
+        self.seed = seed
+
+    def generate_episode(self):
+        # Aggregators
+        episode_observations = []
+        episode_actions = []
+        obs = self.env.reset()
+        # Iteration
+        for timestep in range(self.horizon):
+            # Registering observation
+            episode_observations.append(obs)
+            # Getting action and step
+            a = self.env.action_space.sample()
+            obs, reward, done, info = self.env.step(a)
+            # Registering action
+            episode_actions.append(a)
+            # Check if done
+            if done:
+                break
+        # Numpify
+        episode_observations = np.array(episode_observations)
+        episode_actions = np.array(episode_actions)
+        return episode_observations, episode_actions
+
+    def run(self):
+        # Make env and model
+        self.env = self.make_env(self.seed)
+        self.model = self.make_model()
+        print("Worker %s - Running with seed %s" % (os.getpid(), self.seed))
+        # Start continuous loop
+        while True:
+            self.event.wait()
+            self.event.clear()
+            command, worker_id = self.input.get()
+            if command == 'generate':
+                episode_observations, episode_actions = self.generate_episode()
+                self.output.put((worker_id, episode_observations, episode_actions))
+            elif command = 'exit':
+                self.env.close()
+                break
 
 class ParallelGenerator(object):
 
@@ -52,15 +78,43 @@ class ParallelGenerator(object):
         # Get the number of workers
         self.n_workers = select_n_workers(n_workers)
         print('Using', self.n_workers, 'workers.')
-        # Define the pool
-        self.pool = Pool(self.n_workers)
+        # Define queues, events and the pool of workers
+        self.output_queue = Queue()
+        self.input_queues = [Queue() for _ in range(self.n_workers)]
+        self.events = [Event() for _ in range(self.n_workers)]
+        self.workers = [Worker(self.input_queues[i], self.output_queue, self.events[i],
+                            self.make_env, self.make_model, self.horizon, self.seed+i) for i in range(self.n_workers)]
+        # Start the workers
+        for w in self.workers:
+            w.start()
 
     def generate(self, size):
-        # Apply async
-        results = [self.pool.apply_async(generate_episode, args=(self.make_env, self.make_model, self.seed+i, self.horizon)) for i in range(size)]
-        observations, actions = zip(*[r.get() for r in results])
-        print("Done?")
-        return np.array(observations, actions)
+        total_steps = 0
+        for i in range(self.n_workers):
+            self.input_queues[i].put('generate')
+            self.events[i].set()
+
+        observations = []
+        actions = []
+        while total_steps < size:
+            id, episode_observations, episode_actions = self.output_queue.get()
+            observations.append(episode_observations)
+            actions.append(episode_actions)
+            total_steps += 1
+            if total_steps < (size - self.n_workers+1):
+                self.input_queues[id].put(('generate', id))
+                self.events[id].set()
+
+        return np.array(observations), np.array(actions)
+
+    def close(self):
+        for i in range(self.n_workers):
+            self.input_queues[i].put('exit')
+        for e in self.events:
+            e.set()
+        for w in self.workers:
+            w.join()
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -95,6 +149,8 @@ if __name__ == '__main__':
     observations, actions = generator.generate(args.size)
 
     print(observations.shape, actions.shape)
+
+    generator.close()
 
     '''
     # Numpify global
